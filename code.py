bfeed95c844025363476bfc54364d7f7b00b6f61
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2022-05-03 10:12:56 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,7 @@
 if __name__ == '__main__':
-    n = int(input())
-    arr = map(int, input().split())
-    arr_arngment = sorted(list(arr))
-    print(arr_arngment[-3])
+    n = int(input()) #input n
+    arr = map(int, input().split()) #list in put
+    arr = list(set(list(arr))) #convert the input to list 
+    arr_arngment = sorted(list(arr)) #sort the list 
+    ar = len(arr) #get the length 
+    print(arr_arngment[ar-2])#print thr runner up 
